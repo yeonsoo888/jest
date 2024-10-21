c2cbbf46ec58b662606a86a7a5f66950d0c157bb
yeonsoo888 <yeonsoo9846@gmail.com>
2024-10-21 11:52:17 +0900

    #feat : object 비교

--- src/toBe.spec.ts
+++ src/toBe.spec.ts
@@ -1,6 +1,10 @@
-import {sum} from "./toBe";
+import {sum , obj} from "./toBe";
 
 test('sum 함수는 두 숫자를 더해야 한다',() => {
   expect(sum(2, 5)).toBe(7);
   expect(sum(2, 5)).not.toBe(4);
+})
+
+test('obj 함수는 object를 리턴한다. ',() => {
+  expect(obj()).toStrictEqual({a: 'hello'});
 })
--- src/toBe.ts
+++ src/toBe.ts
@@ -1,3 +1,7 @@
 export function sum(x:number,y:number) {
   return x + y;
 }
+
+export function obj() {
+  return {a: 'hello'};
+}
